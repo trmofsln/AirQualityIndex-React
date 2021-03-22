f63705fa16e6c864a9be1f2458a7773c3d7b3a11
Yash <yashprajapati672@gmail.com>
2021-03-22 14:26:41 +0530

    improve team page by yash

--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -1,8 +1,8 @@
 import React from "react";
 import './OurTeam.css'
 import ramanimg from "../assets/raman6.png"
-import yashimg from "../assets/yash1.png"
-import sanyamimg from "../assets/sanyam1.png"
+import yashimg from "../assets/WhatsApp_Image_2021-03-22_at_1.17.13_PM-removebg.png"
+import sanyamimg from "../assets/WhatsApp_Image_2021-03-22_at_2.05.21_PM-removebg-preview.png"
 
 
 
@@ -11,23 +11,26 @@ const teamDetail = [
   {
     id: "p1",
     name: "Tirupati Raman Mishra",
-    review: "Develop some thing very good. study in b.tech 5th sem and also good web developer",
+    review: "Develop Header and Footer Component , Team Page,design theme for pages and having a good experience in Css and Bootstrap .",
   },
   {
     id: "p2",
     name: "Yash Kumar Prajapati",
-    review: "Develop some thing very good. study in b.tech 5th sem and also good web developer",
+    review: "Develop HomePage,TeamPage, Article Page ,design content for all the pages  and having a good experience in Backend.",
   },
   {
     id: "p3",
     name: "Sanyam Gupta",
-    review: "Develop some thing very good. study in b.tech 5th sem and also good web developer",
+    review: "Develop Article Page and design basic component in react and having a good experience in Frontend.",
   },
 ];
 
 const OurTeam = () => {
   return (
     <div className="ourteam-background">
+    <h1 className="team-bottomh1"> Paper Design Project (Phase-I) </h1>
+    <h1 className="team-bottomh2"> JK Institute of Applied Physics & Technology </h1>
+    <h1 className="team-bottomh3"> University Of Allahabad</h1>
       <h1 className="team-heading"> Our Team </h1>
       <div className="teammate person1">
       <h1 className="teammate-h1"> {teamDetail[0].name} </h1>
@@ -52,9 +55,8 @@ const OurTeam = () => {
       <h1 className="class-detail" > Enroll : U1851056</h1>
       <img className="team-img" src={sanyamimg} />
       </div>
-      <h1 className="team-bottomh1"> Paper Design Project (Phase-I) </h1>
-      <h1 className="team-bottomh2"> JK Institute of Applied Physics & Technology </h1>
-      <h1 className="team-bottomh3"> Submitted To: Dr. Sudhanshu Kumar Jha </h1>
+      <h1 className="supervisor">Supervision By </h1>
+      <h2 className="supervisor-name">Dr. Sudhanshu Kumar Jha Sir </h2>
     </div>
   );
 };
