0b475cfeb07d97af8d156fa551317cc1ecb4d357
Tirupati Raman Mishra <tirupatiramanmishra@gmail.com>
2021-03-23 01:20:48 +0530

    Tirupati improvement in Home Page & Our Team Page

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -5,25 +5,25 @@ import { BackTop } from 'antd';
 const PageFooter = () => {
   return (
     <div className="container-fluid">
-    <div className="footer-flex-css">
-   
-      <div className="footer">
-        <div className="logo">
-          <i className="fas fa-bolt"></i>
-          <a href="/">Air Index</a>
+      <div className="footer-flex-css">
+
+        <div className="footer">
+          <div className="logo">
+            <i className="fas fa-bolt"></i>
+            <a href="/">Air Index</a>
+          </div>
+          <ul className="socials">
+            <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a></li>
+            <li><a href="https://www.twitter.com/"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a></li>
+            <li><a href="https://www.pinterest.com/"><i className="fab fa-pinterest-p"></i></a></li>
+            <li><a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a></li>
+          </ul>
+
+          <BackTop>
+            <div className="goTop"><i className="fas fa-arrow-circle-up"></i></div>
+          </BackTop>
         </div>
-        <ul className="socials">
-          <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a></li>
-          <li><a href="https://www.twitter.com/"><i className="fab fa-twitter"></i></a></li>
-          <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a></li>
-          <li><a href="https://www.pinterest.com/"><i className="fab fa-pinterest-p"></i></a></li>
-          <li><a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a></li>
-        </ul>
-        
-        <BackTop>
-          <div className="goTop"><i className="fas fa-arrow-circle-up"></i></div>
-        </BackTop>
-      </div>
       </div>
       <div className="copyright">Copyright &copy; 2021 airIndex</div>
     </div>
